Orthogonalpub/ode_solvespace

commit d1906c22b6f09284352a17c07c5a1fe7942a3538

Fix paste transformed not correcting tangent arcs when mirrored (#833)

src/constraint.cpp
@@ -127,6 +127,66 @@ hConstraint Constraint::ConstrainCoincident(hEntity ptA, hEntity ptB) {
         Entity::NO_ENTITY, Entity::NO_ENTITY, /*other=*/false, /*other2=*/false);
 }
 
+void Constraint::ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *arc) {
+    Vector l0 = SK.GetEntity(line->point[0])->PointGetNum(),
+           l1 = SK.GetEntity(line->point[1])->PointGetNum();
+    Vector a1 = SK.GetEntity(arc->point[1])->PointGetNum(),
+           a2 = SK.GetEntity(arc->point[2])->PointGetNum();
+    if(l0.Equals(a1) || l1.Equals(a1)) {
+        c->other = false;
+    } else if(l0.Equals(a2) || l1.Equals(a2)) {
+        c->other = true;
+    } else {
+        Error(_("The tangent arc and line segment must share an "
+                "endpoint. Constrain them with Constrain -> "
+                "On Point before constraining tangent."));
+        return;
+    }
+}
+
+void Constraint::ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *cubic) {
+    Vector l0 = SK.GetEntity(line->point[0])->PointGetNum(),
+           l1 = SK.GetEntity(line->point[1])->PointGetNum();
+    Vector as = cubic->CubicGetStartNum(),
+           af = cubic->CubicGetFinishNum();
+
+    if(l0.Equals(as) || l1.Equals(as)) {
+        c->other = false;
+    } else if(l0.Equals(af) || l1.Equals(af)) {
+        c->other = true;
+    } else {
+        Error(_("The tangent cubic and line segment must share an "
+                "endpoint. Constrain them with Constrain -> "
+                "On Point before constraining tangent."));
+        return;
+    }
+}
+
+void Constraint::ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *eB) {
+    Vector as = eA->EndpointStart(),
+           af = eA->EndpointFinish(),
+           bs = eB->EndpointStart(),
+           bf = eB->EndpointFinish();
+    if(as.Equals(bs)) {
+        c->other = false;
+        c->other2 = false;
+    } else if(as.Equals(bf)) {
+        c->other = false;
+        c->other2 = true;
+    } else if(af.Equals(bs)) {
+        c->other = true;
+        c->other2 = false;
+    } else if(af.Equals(bf)) {
+        c->other = true;
+        c->other2 = true;
+    } else {
+        Error(_("The curves must share an endpoint. Constrain them "
+                "with Constrain -> On Point before constraining "
+                "tangent."));
+        return;
+    }
+}
+
 void Constraint::MenuConstrain(Command id) {
     Constraint c = {};
     c.group = SS.GW.activeGroup;
@@ -617,50 +677,22 @@ void Constraint::MenuConstrain(Command id) {
                 c.entityA = gs.vector[0];
                 c.entityB = gs.vector[1];
             } else if(gs.lineSegments == 1 && gs.arcs == 1 && gs.n == 2) {
-                Entity *line = SK.GetEntity(gs.entity[0]);
-                Entity *arc  = SK.GetEntity(gs.entity[1]);
+                Entity *line = SK.GetEntity(gs.entity[0]),
+                       *arc  = SK.GetEntity(gs.entity[1]);
                 if(line->type == Entity::Type::ARC_OF_CIRCLE) {
                     swap(line, arc);
                 }
-                Vector l0 = SK.GetEntity(line->point[0])->PointGetNum(),
-                       l1 = SK.GetEntity(line->point[1])->PointGetNum();
-                Vector a1 = SK.GetEntity(arc->point[1])->PointGetNum(),
-                       a2 = SK.GetEntity(arc->point[2])->PointGetNum();
-
-                if(l0.Equals(a1) || l1.Equals(a1)) {
-                    c.other = false;
-                } else if(l0.Equals(a2) || l1.Equals(a2)) {
-                    c.other = true;
-                } else {
-                    Error(_("The tangent arc and line segment must share an "
-                            "endpoint. Constrain them with Constrain -> "
-                            "On Point before constraining tangent."));
-                    return;
-                }
+                ConstrainArcLineTangent(&c, line, arc);
                 c.type = Type::ARC_LINE_TANGENT;
                 c.entityA = arc->h;
                 c.entityB = line->h;
             } else if(gs.lineSegments == 1 && gs.cubics == 1 && gs.n == 2) {
-                Entity *line  = SK.GetEntity(gs.entity[0]);
-                Entity *cubic = SK.GetEntity(gs.entity[1]);
+                Entity *line  = SK.GetEntity(gs.entity[0]),
+                       *cubic = SK.GetEntity(gs.entity[1]);
                 if(line->type == Entity::Type::CUBIC) {
                     swap(line, cubic);
                 }
-                Vector l0 = SK.GetEntity(line->point[0])->PointGetNum(),
-                       l1 = SK.GetEntity(line->point[1])->PointGetNum();
-                Vector as = cubic->CubicGetStartNum(),
-                       af = cubic->CubicGetFinishNum();
-
-                if(l0.Equals(as) || l1.Equals(as)) {
-                    c.other = false;
-                } else if(l0.Equals(af) || l1.Equals(af)) {
-                    c.other = true;
-                } else {
-                    Error(_("The tangent cubic and line segment must share an "
-                            "endpoint. Constrain them with Constrain -> "
-                            "On Point before constraining tangent."));
-                    return;
-                }
+                ConstrainCubicLineTangent(&c, line, cubic);
                 c.type = Type::CUBIC_LINE_TANGENT;
                 c.entityA = cubic->h;
                 c.entityB = line->h;
@@ -671,24 +703,7 @@ void Constraint::MenuConstrain(Command id) {
                 }
                 Entity *eA = SK.GetEntity(gs.entity[0]),
                        *eB = SK.GetEntity(gs.entity[1]);
-                Vector as = eA->EndpointStart(),
-                       af = eA->EndpointFinish(),
-                       bs = eB->EndpointStart(),
-                       bf = eB->EndpointFinish();
-                if(as.Equals(bs)) {
-                    c.other = false; c.other2 = false;
-                } else if(as.Equals(bf)) {
-                    c.other = false; c.other2 = true;
-                } else if(af.Equals(bs)) {
-                    c.other = true; c.other2 = false;
-                } else if(af.Equals(bf)) {
-                    c.other = true; c.other2 = true;
-                } else {
-                    Error(_("The curves must share an endpoint. Constrain them "
-                            "with Constrain -> On Point before constraining "
-                            "tangent."));
-                    return;
-                }
+                ConstrainCurveCurveTangent(&c, eA, eB);
                 c.type = Type::CURVE_CURVE_TANGENT;
                 c.entityA = eA->h;
                 c.entityB = eB->h;

src/sketch.h
@@ -790,6 +790,9 @@ public:
     static hConstraint TryConstrain(Constraint::Type type, hEntity ptA, hEntity ptB,
                                     hEntity entityA, hEntity entityB = Entity::NO_ENTITY,
                                     bool other = false, bool other2 = false);
+    static void ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *arc);
+    static void ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *cubic);
+    static void ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *eB);
 };
 
 class hEquation {

src/clipboard.cpp
@@ -245,18 +245,41 @@ void GraphicsWindow::PasteClipboard(Vector trans, double theta, double scale) {
         c.reference = cc->reference;
         c.disp = cc->disp;
         c.comment = cc->comment;
-        bool removeConstraint = false;
+        bool dontAddConstraint = false;
         switch(c.type) {
             case Constraint::Type::COMMENT:
                 c.disp.offset = c.disp.offset.Plus(trans);
                 break;
-
             case Constraint::Type::PT_PT_DISTANCE:
             case Constraint::Type::PT_LINE_DISTANCE:
             case Constraint::Type::PROJ_PT_DISTANCE:
             case Constraint::Type::DIAMETER:
                 c.valA *= fabs(scale);
                 break;
+            case Constraint::Type::ARC_LINE_TANGENT: {
+                Entity *line = SK.GetEntity(c.entityB),
+                       *arc  = SK.GetEntity(c.entityA);
+                if(line->type == Entity::Type::ARC_OF_CIRCLE) {
+                    swap(line, arc);
+                }
+                Constraint::ConstrainArcLineTangent(&c, line, arc);
+                break;
+            }
+            case Constraint::Type::CUBIC_LINE_TANGENT: {
+                Entity *line  = SK.GetEntity(c.entityB),
+                       *cubic = SK.GetEntity(c.entityA);
+                if(line->type == Entity::Type::CUBIC) {
+                    swap(line, cubic);
+                }
+                Constraint::ConstrainCubicLineTangent(&c, line, cubic);
+                break;
+            }
+            case Constraint::Type::CURVE_CURVE_TANGENT: {
+                Entity *eA = SK.GetEntity(c.entityA),
+                       *eB = SK.GetEntity(c.entityB);
+                Constraint::ConstrainCurveCurveTangent(&c, eA, eB);
+                break;
+            }
             case Constraint::Type::HORIZONTAL:
             case Constraint::Type::VERTICAL:
                 // When rotating 90 or 270 degrees, swap the vertical / horizontal constaints
@@ -267,13 +290,13 @@ void GraphicsWindow::PasteClipboard(Vector trans, double theta, double scale) {
                         c.type = Constraint::Type::HORIZONTAL;
                     }
                 } else if (fmod(theta, PI/2) != 0) {
-                    removeConstraint = true;
+                    dontAddConstraint = true;
                 }
                 break;
             default:
                 break;
         }
-        if (!removeConstraint) {
+        if (!dontAddConstraint) {
             hConstraint hc = Constraint::AddConstraint(&c, /*rememberForUndo=*/false);
             if(c.type == Constraint::Type::COMMENT) {
                 MakeSelected(hc);

commit d62ea3ee1380eedcb18fd400fb781e271921c9e8

Fix tangent constraints of curves.
This fixes a regression introduced in  d1906c22b6f09284352a17c07c5a1fe7942a3538
(https://github.com/solvespace/solvespace/pull/833.)
Three `return` statements got "swallowed" by the newly created functions.
(e.g .https://github.com/solvespace/solvespace/commit/d1906c22b6f09284352a17c07c5a1fe7942a3538#diff-49abc03ed071148c0ebae0c64aafb625fa6223135f77aeecdb47dab4cf8b940cL638 )

Because of this it was possible to constrain arcs and cubics tangent to
each other or line segments, without them sharing an endpoint.

This kind of worked, but always chose the "starting" points of the curves
and lines. In the future this can be turned into a feature. See the
discussion in #937.

src/constraint.cpp
@@ -127,7 +127,7 @@ hConstraint Constraint::ConstrainCoincident(hEntity ptA, hEntity ptB) {
         Entity::NO_ENTITY, Entity::NO_ENTITY, /*other=*/false, /*other2=*/false);
 }
 
-void Constraint::ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *arc) {
+bool Constraint::ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *arc) {
     Vector l0 = SK.GetEntity(line->point[0])->PointGetNum(),
            l1 = SK.GetEntity(line->point[1])->PointGetNum();
     Vector a1 = SK.GetEntity(arc->point[1])->PointGetNum(),
@@ -140,11 +140,12 @@ void Constraint::ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *ar
         Error(_("The tangent arc and line segment must share an "
                 "endpoint. Constrain them with Constrain -> "
                 "On Point before constraining tangent."));
-        return;
+        return false;
     }
+    return true;
 }
 
-void Constraint::ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *cubic) {
+bool Constraint::ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *cubic) {
     Vector l0 = SK.GetEntity(line->point[0])->PointGetNum(),
            l1 = SK.GetEntity(line->point[1])->PointGetNum();
     Vector as = cubic->CubicGetStartNum(),
@@ -158,11 +159,12 @@ void Constraint::ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *
         Error(_("The tangent cubic and line segment must share an "
                 "endpoint. Constrain them with Constrain -> "
                 "On Point before constraining tangent."));
-        return;
+        return false;
     }
+    return true;
 }
 
-void Constraint::ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *eB) {
+bool Constraint::ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *eB) {
     Vector as = eA->EndpointStart(),
            af = eA->EndpointFinish(),
            bs = eB->EndpointStart(),
@@ -183,8 +185,9 @@ void Constraint::ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *e
         Error(_("The curves must share an endpoint. Constrain them "
                 "with Constrain -> On Point before constraining "
                 "tangent."));
-        return;
+        return false;
     }
+    return true;
 }
 
 void Constraint::MenuConstrain(Command id) {
@@ -690,7 +693,9 @@ void Constraint::MenuConstrain(Command id) {
                 if(line->type == Entity::Type::ARC_OF_CIRCLE) {
                     swap(line, arc);
                 }
-                ConstrainArcLineTangent(&c, line, arc);
+                if(!ConstrainArcLineTangent(&c, line, arc)) {
+                    return;
+                }
                 c.type = Type::ARC_LINE_TANGENT;
                 c.entityA = arc->h;
                 c.entityB = line->h;
@@ -700,7 +705,9 @@ void Constraint::MenuConstrain(Command id) {
                 if(line->type == Entity::Type::CUBIC) {
                     swap(line, cubic);
                 }
-                ConstrainCubicLineTangent(&c, line, cubic);
+                if(!ConstrainCubicLineTangent(&c, line, cubic)) {
+                    return;
+                }
                 c.type = Type::CUBIC_LINE_TANGENT;
                 c.entityA = cubic->h;
                 c.entityB = line->h;
@@ -711,7 +718,9 @@ void Constraint::MenuConstrain(Command id) {
                 }
                 Entity *eA = SK.GetEntity(gs.entity[0]),
                        *eB = SK.GetEntity(gs.entity[1]);
-                ConstrainCurveCurveTangent(&c, eA, eB);
+                if(!ConstrainCurveCurveTangent(&c, eA, eB)) {
+                    return;
+                }
                 c.type = Type::CURVE_CURVE_TANGENT;
                 c.entityA = eA->h;
                 c.entityB = eB->h;

src/sketch.h
@@ -790,9 +790,9 @@ public:
     static hConstraint TryConstrain(Constraint::Type type, hEntity ptA, hEntity ptB,
                                     hEntity entityA, hEntity entityB = Entity::NO_ENTITY,
                                     bool other = false, bool other2 = false);
-    static void ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *arc);
-    static void ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *cubic);
-    static void ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *eB);
+    static bool ConstrainArcLineTangent(Constraint *c, Entity *line, Entity *arc);
+    static bool ConstrainCubicLineTangent(Constraint *c, Entity *line, Entity *cubic);
+    static bool ConstrainCurveCurveTangent(Constraint *c, Entity *eA, Entity *eB);
 };
 
 class hEquation {